969f3340668fa826d5e0774b136c4ac9d1a39902
KaiSej007 <kailyubo@gmail.com>
2025-04-29 12:17:38 +0200

    added model files and db connection

---
+++ src/DBCredentials.php
@@ -0,0 +1,13 @@
+<?php
+namespace Src;
+
+Class DBcredentials
+{
+  protected string $host = '';
+  protected string $dbname = '';
+
+  protected string $user = '';
+
+  protected string $password = '';
+
+}
